8ee2dc0257b9983598fe054990c54040c774f49b
tea-mtt <56030618+tea-mtt@users.noreply.github.com>
2019-10-01 15:45:12 +0200

    Add files via upload

---
+++ atelier 1.R
@@ -0,0 +1,33 @@
+2 + 3
+8 - 12
+4 * sqrt(2)
+4  + 3
+x <- 2
+x + 2
+sin(x)
+sin(2)
+y<-sin(x)
+2+x+y
+exp(1)
+pi
+x = 2 
+2-> x 
+4 -> z
+# variables
+tailles <- c(167, 192, 173, 174, 172, 167, 171, 185, 163, 170)
+length(tailles) #taille echantillon
+mean(tailles) #moyenne
+sd(tailles) #ecart-type
+var(tailles) #variance 
+summary(tailles) #resumé
+hist(tailles) #histogramme 
+poids <- c(86, 74, 83, 50, 78, 66, 66, 51, 50, 55)
+length(poids)
+tailles.st <- (tailles-mean(tailles))/sd(tailles)
+mean(tailles.st)
+sd(tailles.st)
+tailles.st
+tailles.m <- tailles/100
+tailles.m
+imc <- poids/tailles.m^2
+imc
